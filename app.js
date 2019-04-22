06800993c6e1baf2a609ce71ee5af205a5315799
SuverovDmitriy <suverov.dmitriy@gmail.com>
2019-04-23 00:42:28 +0300

    project init

---
+++ app.js
@@ -0,0 +1,26 @@
+const Koa = require("koa");
+const logger = require("koa-logger");
+
+const Router = require("koa-router");
+const app = new Koa();
+const router = new Router();
+app.use(logger());
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit("error", err, ctx);
+  }
+});
+
+router.get("/", (ctx, next) => {
+  ctx.body = "Hello World!";
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const server = app.listen(3000);
+module.exports = server;
